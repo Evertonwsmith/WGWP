2eac72717cc7020507c434fe59199be844d3afe6
Everton Smith <evertonwsmith@gmail.com>
2024-02-08 14:23:14 -0800

    working live

--- android/app/src/main/kotlin/com/example/wordgamewithpals/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.wordgamewithpals
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:wordgamewithpals/firebase_options.dart';
 import 'package:wordgamewithpals/landing.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: "AIzaSyD2pL_AOQFfbg_9bNk_Aes6ETqoVpYLbRY",
-          authDomain: "wordgamewithpals.firebaseapp.com",
-          projectId: "wordgamewithpals",
-          storageBucket: "wordgamewithpals.appspot.com",
-          messagingSenderId: "541477768814",
-          appId: "1:541477768814:web:16ed741d894883d503a07f",
-          measurementId: "G-1NCKFZWYT0"));
+      options: DefaultFirebaseOptions.currentPlatform);
+      // FirebaseOptions(
+      //     apiKey: "AIzaSyD2pL_AOQFfbg_9bNk_Aes6ETqoVpYLbRY",
+      //     authDomain: "wordgamewithpals.firebaseapp.com",
+      //     projectId: "wordgamewithpals",
+      //     storageBucket: "wordgamewithpals.appspot.com",
+      //     messagingSenderId: "541477768814",
+      //     appId: "1:541477768814:web:16ed741d894883d503a07f",
+      //     measurementId: "G-1NCKFZWYT0"));
   runApp(const MyApp());
 }
 
